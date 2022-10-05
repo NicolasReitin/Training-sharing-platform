46693359b18f6690cb95dcecb7baa29a26a66c0e
Nicolas REITIN <reitin.pro@gmail.com>
2022-10-05 11:56:27 +0200

    roles by middlewares

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/private', function () {
+        return 'Bonjour Admin';
+        // return view('Auth/dashboard');
+    });
+});
